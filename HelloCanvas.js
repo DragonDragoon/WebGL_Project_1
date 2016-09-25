2920c43076aab0abf3bb7aa460362a4b3f5ad96b
William Woodard <wwoodar1@uncc.edu>
2016-09-25 19:20:24 -0400

    -completed exercise 4)b)iii) - The World's ...

--- HelloCanvas.js
+++ HelloCanvas.js
@@ -8,10 +8,12 @@ function main() {
   if (!gl) {
     console.log('Failed to get the rendering context for WebGL');
     return;
+  } else {
+    console.log('Successfully created WebGL context');
   }
 
   // Set clear color
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearColor(1.0, 0.0, 0.0, 1.0);
 
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
