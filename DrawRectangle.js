fcdcf7fe5607d259650019b4a6028af490735edc
William Woodard <William Woodard>
2016-09-25 17:16:50 -0400

    -exercise 4.a.i and 4.a.ii

--- DrawRectangle.js
+++ DrawRectangle.js
@@ -10,7 +10,7 @@ function main() {
   // Get the rendering context for 2DCG
   var ctx = canvas.getContext('2d');
 
-  // Draw a blue rectangle
-  ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
-  ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+  // Draw a black rectangle
+  ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set color to black
+  ctx.fillRect(120, 10, 150 * 1.5, 150);        // Fill a rectangle with the color
 }
