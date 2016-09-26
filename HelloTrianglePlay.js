db7b847edec9d5372e419283aacc8ce92126bd27
William Woodard <wwoodar1@uncc.edu>
2016-09-26 01:22:36 -0400

    -exercise 7)d)ii) Triangle Play

--- HelloTrianglePlay.js
+++ HelloTrianglePlay.js
@@ -8,8 +8,10 @@ var VSHADER_SOURCE =
 
 // Fragment shader program
 var FSHADER_SOURCE =
+  'precision mediump float;\n' +
+  'uniform vec4 u_FragColor;\n' +
   'void main() {\n' +
-  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
+  '  gl_FragColor = u_FragColor;\n' +
   '}\n';
 
 function main() {
@@ -29,29 +31,41 @@ function main() {
     return;
   }
 
-  // Write the positions of vertices to a vertex shader
-  var n = initVertexBuffers(gl);
-  if (n < 0) {
-    console.log('Failed to set the positions of the vertices');
-    return;
-  }
-
   // Specify the color for clearing <canvas>
   gl.clearColor(0, 0, 0, 1);
 
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  // Draw the rectangle
-  gl.drawArrays(gl.TRIANGLES, 0, n);
-}
-
-function initVertexBuffers(gl) {
-  var vertices = new Float32Array([
-    0, 0.5,   -0.5, -0.5,   0.5, -0.5
-  ]);
+  var vertices = [
+    new Float32Array([
+      0.25, 0.75, -0.25, 0.25, 0.25, 0.25
+    ]), // Triangle 1
+    new Float32Array([
+      -0.25, -0.75, -0.25, -0.25, 0.25, -0.25
+    ])  // Triangle 2
+  ];
   var n = 3; // The number of vertices
 
+  var colors = [
+    new Float32Array([0.0, 0.0, 1.0, 1.0]),   // Triangle 1
+    new Float32Array([1.0, 0.0, 0.0, 1.0])    // Triangle 2
+  ];
+
+  for (var i = 0; i < vertices.length; i++) {
+    // Write the positions of vertices to a vertex shader
+    var n_v = initVertexBuffers(gl, vertices[i], n, colors[i]);
+    if (n_v < 0) {
+      console.log('Failed to set the positions of the vertices');
+      return;
+    }
+
+    // Draw the rectangle
+    gl.drawArrays(gl.TRIANGLES, 0, n_v);
+  }
+}
+
+function initVertexBuffers(gl, vertices, n, color) {
   // Create a buffer object
   var vertexBuffer = gl.createBuffer();
   if (!vertexBuffer) {
@@ -74,6 +88,16 @@ function initVertexBuffers(gl) {
 
   // Enable the assignment to a_Position variable
   gl.enableVertexAttribArray(a_Position);
+
+  // Get the storage location of u_FragColor
+  var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+  if (!u_FragColor) {
+    console.log('Failed to get u_FragColor variable');
+    return;
+  }
+
+  // Specify the color of u_FragColor
+  gl.uniform4fv(u_FragColor, color);
 
   return n;
 }
